fc3082a99eba87c44a7f00a3087d0a39adb1423a
Sameer Khavanekar <sameer.khavanekar@gmail.com>
2018-08-22 13:02:32 -0700

    Fix

--- Source/Configuration/ColorList.swift
+++ Source/Configuration/ColorList.swift
@@ -7,7 +7,7 @@ public struct ColorList {
     public static var dragIndicator = UIColor(red:0.90, green:0.90, blue:0.90, alpha:1)
     public static var title = UIColor.white
     public static var subtitle = UIColor.white
-  }s
+  }
 
   public struct Whistle {
     public static var background = UIColor.white
